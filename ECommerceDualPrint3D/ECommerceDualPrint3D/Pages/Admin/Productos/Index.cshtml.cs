afb7c099960f26bfe06e9ca644afcb4497ec2af4
Anthony Tolentino <tolentino.git@gmail.com>
2025-10-19 18:32:51 -0300

    Se terima de crear el index de los productos reciclando todo del index de categoria

--- ECommerceDualPrint3D/ECommerceDualPrint3D/Pages/Admin/Productos/Index.cshtml.cs
+++ ECommerceDualPrint3D/ECommerceDualPrint3D/Pages/Admin/Productos/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using ECommerce.DataAccess.Repository.IRepository;
+using ECommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace ECommerceDualPrint3D.Pages.Admin.Productos
 {
     public class IndexModel : PageModel
     {
+        //Inyeccion de dependencias
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IEnumerable<Producto> Productos { get; set; } = default;
         public void OnGet()
         {
+            //Cargamos todas las categorias desde la base de datos
+            Productos = _unitOfWork.Producto.GetAll();
         }
     }
 }
+
--- ECommerceDualPrint3D/ECommerceDualPrint3D/obj/Debug/net8.0/ECommerceDualPrint3D.AssemblyInfo.cs
+++ ECommerceDualPrint3D/ECommerceDualPrint3D/obj/Debug/net8.0/ECommerceDualPrint3D.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceDualPrint3D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eca589ad6bbf67e941b66a8aa929dec4dac365d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db32b87618f51b27eada9770e2db2e83519a1f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceDualPrint3D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceDualPrint3D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
